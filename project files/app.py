0c7649549745bcc90713bf493fb7c96ef9d76da8
SeethaAnusha <146526867+SeethaAnusha@users.noreply.github.com>
2023-11-09 14:25:21 +0530

    Add files via upload

---
+++ project files/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+
+
+sales = Flask(__name__) # initializng the flask app
+
+
+@sales.route('/')
+def helloworld():
+    return render_template("index.html") 
+
+if __name__ == '__main__':
+    sales.run(debug = False, port = 5500)
